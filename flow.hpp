093bc5d1fd603f0436190bb0e8bd899f451e2d3d
iannisdezwart <iannis__@hotmail.com>
2021-01-23 21:27:40 +0100

    🚀 Added New Stuff To Bulk Include File

--- flow.hpp
+++ flow.hpp
@@ -2,6 +2,10 @@
 #define FLOW_HEADER
 
 #include "data-structures/data-structures.hpp"
+#include "data-structures/buffer.hpp"
+#include "data-structures/dynamic-array.hpp"
+#include "data-structures/string.hpp"
 #include "events/event_emitter.hpp"
+#include "formatting/ansi.hpp"
 
 #endif
